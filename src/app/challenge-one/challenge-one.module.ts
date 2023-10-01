774432f09db885fbf3a22d48aedd8b1dac307e2e
Jean Zapata <jeanza94@hotmail.com>
2023-10-01 12:24:13 -0500

    logica challenge one terminada

--- src/app/challenge-one/challenge-one.module.ts
+++ src/app/challenge-one/challenge-one.module.ts
@@ -1,6 +1,7 @@
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ChallengeOneRoutingModule } from './challenge-one-routing.module';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { FormComponent } from './components/form/form.component';
@@ -10,6 +11,7 @@ import { ExamplesComponent } from './components/examples/examples.component';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ChallengeOneRoutingModule
   ],
   declarations: [
--- src/app/challenge-one/components/form/form.component.ts
+++ src/app/challenge-one/components/form/form.component.ts
@@ -1,5 +1,6 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'challenge-one-form',
@@ -8,4 +9,58 @@ import { Component, OnInit } from '@angular/core';
 
 export class FormComponent {
 
+  private formBuilder = inject(FormBuilder)
+
+  public myForm: FormGroup = this.formBuilder.group({
+    test: ['1,2,3,4,5,6', [Validators.required, Validators.minLength(1)]],
+    sNumber: ['6', [Validators.required]],
+  })
+
+  getArrayFromTest(value: string): number[] {
+    const testArr = value.trim().split(',').map(el => +el)
+    return testArr
+  }
+
+  invertArr(arrNumber: number[]) {
+    const invertedArr: number[] = []
+    for (let i = arrNumber.length - 1; i >= 0; i--) {
+      invertedArr.push(arrNumber[i])
+    }
+    return invertedArr
+  }
+
+  removeNumberFromArray = (s: number, arrNumber: number[]) => {
+    const removeNumberArr = arrNumber.map(num => {
+      const numStr = num.toString()
+      if (numStr.length > 1) {
+        const arrStrNum = numStr.split("").map(n => {
+          if (+n >= s) return ""
+          return n
+        })
+        const newStrNumber = arrStrNum.toString().replace(/,/g, "")
+        return newStrNumber === "" ? null : +newStrNumber
+      }
+
+      if (num >= s) return null
+
+      return num
+    })
+
+    return removeNumberArr.filter(num => num !== null)
+
+  }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      //todo errores
+      return
+    }
+    const test: string = this.myForm.get('test')!.value
+    const s: number = +this.myForm.get('sNumber')!.value
+
+    const testArr = this.getArrayFromTest(test)
+    const invertedArr = this.invertArr(testArr)
+    const result = this.removeNumberFromArray(s, invertedArr)
+    console.log(result)
+  }
 }
