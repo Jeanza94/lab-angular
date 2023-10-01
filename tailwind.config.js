f6a8d142e6f7f273fd14f3b980db75b02e7fc038
Jean Zapata <jeanza94@hotmail.com>
2023-10-01 11:05:45 -0500

    configuración de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        common: {
+          light: "theme(colors.neutral.100)",
+          base: "theme(colors.neutral.600)",
+          dark: "theme(colors.neutral.900)",
+        },
+        primary: {
+          light: "theme(colors.blue.100)",
+          base: "theme(colors.blue.600)",
+          dark: "theme(colors.blue.900)",
+        },
+        secondary: {
+          light: "theme(colors.yellow.100)",
+          base: "theme(colors.yellow.500)",
+          dark: "theme(colors.yellow.900)",
+        },
+        terciary: {
+          light: "theme(colors.red.100)",
+          base: "theme(colors.red.600)",
+          dark: "theme(colors.red.900)",
+        },
+      }
+    },
   },
   plugins: [],
 }
