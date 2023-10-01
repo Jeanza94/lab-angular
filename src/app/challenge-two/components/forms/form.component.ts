858823b75a48b6edecb9ad99f49c5f40af0c2e67
Jean Zapata <jeanza94@hotmail.com>
2023-10-01 18:38:56 -0500

    validaciones en challenge two

--- src/app/challenge-two/components/forms/form.component.ts
+++ src/app/challenge-two/components/forms/form.component.ts
@@ -1,7 +1,8 @@
 
 import { Component, inject, signal, computed } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { ChallengeTwoService } from '../../services/challenge-two.service';
+import { ValidationsService } from 'src/app/shared/services/validations.service';
 
 @Component({
   selector: 'challenge-two-form',
@@ -11,18 +12,36 @@ import { ChallengeTwoService } from '../../services/challenge-two.service';
 export class FormComponent {
 
 
-
   private challTwoService = inject(ChallengeTwoService)
   private formBuilder = inject(FormBuilder)
+  private validationService = inject(ValidationsService)
 
   private answer = signal<number[] | null>(null)
 
   public answerGetter = computed(() => this.answer())
 
   public myForm: FormGroup = this.formBuilder.group({
-    test: ['1, 2, 3, 5, 6, 8, 9', [Validators.required, Validators.minLength(1)]],
-    sNumber: ['6', [Validators.required]],
+    test: ['1, 2, 3, 5, 6, 8, 9', [Validators.required, this.validationTest]],
+    sNumber: ['6', [Validators.required, this.validationService.validationS]],
   })
+
+  validationTest(control: AbstractControl) {
+    const value:string = control.value
+
+    const isNotNumber= value.trim().split(',').some(elem => isNaN(+elem))
+    if (isNotNumber) return {notNumber: true}
+
+    return null
+  }
+
+  isError(control: AbstractControl) {
+    return this.validationService.isError(control)
+  }
+
+  getMessageError(error: {[key: string]: any} | null) {
+    if (!error) return null
+    return this.validationService.getMessageError(error)
+  }
 
   getArrayFromTest(value: string): number[] {
     const testArr = value.trim().split(',').map(el => +el)
@@ -32,6 +51,7 @@ export class FormComponent {
   onSubmit() {
     if (this.myForm.invalid) {
       //todo errores
+      this.myForm.markAllAsTouched()
       return
     }
     const test: string = this.myForm.get('test')!.value
